22126ab88cb88df5792bec1e3d28fb01ece495e0
Jojanyu <Johan.vargas>
2022-09-13 11:02:48 -0500

    Added BigInteger support.

--- calculator.groovy
+++ calculator.groovy
@@ -1,23 +1,24 @@
-/* Hello World in Groovy */
 Scanner scan = new Scanner(System.in);
+def result
 print("Ingrese el primer numero: ")
-def numberA =  System.in.newReader().readLine() as Integer
+def numberA =  System.in.newReader().readLine() as BigInteger
 print("Ingrese el segundo numero: ")
-def numberB =  System.in.newReader().readLine() as Integer
+def numberB =  System.in.newReader().readLine() as BigInteger
 print("Ingrese la expresión ( [ * ][ / ][ - ][ + ] ) : ")
 expression = System.in.newReader().readLine()
+
 switch(expression) { 
    case "*": 
-        println(numberA*numberB)
+        result = numberA*numberB
         break;
    case "+": 
-        println(numberA+numberB) 
+        result = numberA+numberB 
         break;
    case "-": 
-        println(numberA-numberB)
+        result = numberA-numberB
         break;
    case "/":
-        println(numberA/numberB)
+	result = numberA/numberB
         break;
     default:
         println("No hay un operador válido")
